d13cca09ec81953c9d7633dda804968b3639c1b3
Seungyeon Baek <72596522+yeonannab@users.noreply.github.com>
2023-01-02 21:04:44 +0900

    Fix: update model schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,20 +1,10 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
     user_id: {
         type: String,
         required: true
     },
-    groups_own: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Group'
-        }
-    ],
     groups_in: [
         {
             type: mongoose.Schema.Types.ObjectId,
